a890ec43d39565a791eaa7bb568c934ff887154b
Shahmir Javaid <shahmirj@gmail.com>
2016-06-03 18:10:55 +0100

    PS4 controller working and sending data to dashee

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # client-ps4
 The PS4 controller sending info up to the dashee server
+
+*Note, this only works on the PS4 controller using USB. Tested on 
+MAC OSX only.*
+
+Install the packages required
+
+    npm install
+
+To run
+
+    node ps4.js
+    
+You can specify the host (default localhost) or port (default 2047) 
+by using:
+
+    node ps4.js --host 192.168.1.76 --port 1337
--- ps4.js
+++ ps4.js
@@ -1,44 +1,71 @@
 var ds4 = require('ds4');
 var hid = require('node-hid');
-var through = require('through');
+var dgram = require('dgram');
+var cla = require('command-line-args');
 
-console.log(hid.devices());
+// Set the arguments
+GLOBAL.arguments = cla([
+    { name: 'host', alias: 'h', type: String, defaultValue: "localhost" },
+    { name: 'port', alias: 'p', type: Number, defaultValue: 2047 }
+]);
+
+//console.log(hid.devices());
+var client = dgram.createSocket('udp4');
 device = new hid.HID(1356, 1476);
 
+// Helpful logs to determine
+console.log("Connecting to host " + GLOBAL.arguments.host + ":" + GLOBAL.arguments.port)
+console.log("Opened device 1356, 1476");
+
+/**
+ * On each data that comes back from the controller send to
+ * the server
+ */
 device.on('data', function(buf) {
     data = ds4.parseDS4HIDData(buf.slice(0));
+    
+    var message = new Buffer([
+        0, // This is telling the server I am sending a settarget command
+        data.leftAnalogY,
+        data.leftAnalogX,
+        data.rightAnalogX,
+        data.rightAnalogY
+    ]);
 
-    if (data.cross) {
-        ds4.emit(device, {
-            r: 0,
-            g: 0,
-            b: 255
-        })
+    var toStickData = {
+        red: 0,
+        green: 0,
+        blue: 0,
+        flashOn: 0,
+        flashOff: 0,
+        rumbleRight: 0,
+        rumbleLeft: 0
+    };
+
+    // Send to the server
+    client.send(message, 0, message.length, GLOBAL.arguments.port, GLOBAL.arguments.host);
+
+    // Add vibration when the throttle is closer to max
+    if (data.rightAnalogY > 240) {
+        toStickData.rumbleLeft = 200;
+        toStickData.rumbleRight = 200;
+    } else if (data.rightAnalogY > 230) {
+        toStickData.rumbleLeft = 150;
+        toStickData.rumbleRight = 150;
+    } else if (data.rightAnalogY > 200) {
+        toStickData.rumbleLeft = 100;
+        toStickData.rumbleRight = 100;
     }
 
-    if (data.circle) {
-        ds4.emit(device, {
-            r: 255,
-            g: 0,
-            b: 0
-        })
-    }
+    ds4.emit(device, {
+        r: toStickData.red,
+        g: toStickData.green,
+        b: toStickData.blue,
+        flashOn: toStickData.flashOn,
+        flashOff: toStickData.flashOff,
+        rumbleRight: toStickData.rumbleRight,
+        rumbleLeft: toStickData.rumbleLeft
+    });
 
-    if (data.triangle) {
-        ds4.emit(device, {
-            r: 0,
-            g: 255,
-            b: 0
-        })
-    }
-
-    if (data.square) {
-        ds4.emit(device, {
-            r: 255,
-            g: 100,
-            b: 153
-        })
-    }
-
-    console.log(ds4.parseDS4HIDData(buf));
+    console.log(data.leftAnalogY + "p " + data.leftAnalogX + "r "  + data.rightAnalogX + "y " + data.rightAnalogY + "t")
 });
